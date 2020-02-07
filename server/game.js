15a62f372d57e4c1d97a72dd0702a960c02ffc0d
graemsheppard <graemsheppard@gmail.com>
2020-02-07 09:54:27 -0500

    commented server side code

--- server/game.js
+++ server/game.js
@@ -1,11 +1,11 @@
 
-class Game{
+class Game {
   constructor(p1, p2){ //p1 is the player that was waiting,  p2 is the player that just entered
     this.players = [p1, p2];
     this.choices = [null, null];
     this.score = [0, 0];
 
-
+    // Add event listeners to check when players make their choice
     this.players.forEach((player, idx) => {
       player.on('chosen', (id) => {
           this.players.forEach((player) => {
@@ -17,6 +17,7 @@ class Game{
       });
     });
 
+    // Add event listeners to check for player messages
     this.players.forEach((player)=>{
       player.on("message", (text)=>{
         this.__sendMessageToPlayers(text);
@@ -26,16 +27,17 @@ class Game{
     this.__sendMessageToPlayers("Server: The game can start");
   }
 
-  __display(idx, id){
-
+  // Tell sockets if the enemy has chosen
+  __display(idx, id) {
     if(idx == 0){
       this.players[1].emit("enemyChosen", id);
     }
-    else{
+    else {
       this.players[0].emit("enemyChosen", id);
     }
   }
 
+  // Stores the choices of both players
   makeChoice (player, choice) {
     if (player == this.players[0])
       this.choices[0] = choice;
@@ -49,6 +51,8 @@ class Game{
       if (this.choices[0] == this.choices[1]) {
         console.log("Tie");
       } else {
+
+        // Decides the winner
         var winner = -1;
         switch (this.choices[0] + this.choices[1]) {
           case "rockpaper":
@@ -76,11 +80,16 @@ class Game{
             winner = 0;
             break;
         }
-        this.score[winner] += 1;
 
+        // Increase the winner's score by 1
+        this.score[winner] += 1;
       }
+
+      // Reset game
       this.choices[0] = null;
       this.choices[1] = null;
+
+      // Send the score to clients
       this.players.forEach((player, idx) => {
         if(player == this.players[1]){
           player.emit('score', this.score[1], this.score[0]);
--- server/server.js
+++ server/server.js
@@ -1,40 +1,44 @@
 //****Assignment 1***
 
+// Dependancies
+
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Game = require('./game.js');
-
-
-
 const app = express();
 
+// Assign the working directory
 const clientPath = `${__dirname}/../Client`; //Note you have to use these quotes ``
 console.log(`serving static from ${clientPath}`);
-
 app.use(express.static(clientPath));
 
+// Create the server
 const server = http.createServer(app);
-
 const io = socketio(server);
 
+// Declare variables
 var waitingPlayer = null;
-
 var game;
 var chat;
+
+// Socket events
 io.on('connection', (sock) => {
+
+  // If there is already a player create a game
   if(waitingPlayer){
     game = new Game(waitingPlayer, sock);
-    
     console.log("game can start");
     waitingPlayer = null;
-
   }
-  else{
+
+  // If there is no waiting player assign value of socket to it
+  else {
     waitingPlayer = sock;
     console.log("waiting for player");
   }
 
+  // When a waiting player disconnects set it to null
   sock.on('disconnect', () => {
     if (waitingPlayer == sock)
       waitingPlayer = null;
